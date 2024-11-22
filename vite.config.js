471a47a86264c7b7608c5388f6e5dae2da17a892
Cata <catalin.fertu@gmaill.com>
2024-11-22 13:43:18 +0200

    Add vite config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true, // Expose to all network interfaces
-    port: 5173, // Default Vite port
-  },
+  base: '/loopweb/',
 })
